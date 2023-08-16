256da934a5d89b35cdb8d466c2cafbf77fb689b1
Cassius Reynolds <cassius2828@gmail.com>
2023-08-15 22:49:58 -0700

    very very basic skeleton laid out for mobile design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+import {
+  faBoltLightning,
+  faBrain,
+  faComment,
+  faEye,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-contaienr">
+        <Results/>
+        <Summary/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+export const Results = () => {
+  return (
+    <div className="results-container">
+      <h5>your result</h5>
+      <div>
+        <h1>76</h1>
+        <p>of 100</p>
+      </div>
+      <h3>Great</h3>
+      <p>lorem sati hale fhns ifodhsa nfdksjlahf fhdsuf s fndsk hfdso fhs</p>
+    </div>
+  );
+};
+
+export const Summary = () => {
+  return (
+    <div className="summary-container">
+      <h4></h4>
+      <div>
+        <div class="score-rows">
+          <FontAwesomeIcon style={{ color: "red" }} icon={faBoltLightning} />
+        </div>
+        <div class="score-rows">
+          <FontAwesomeIcon style={{ color: "yellow" }} icon={faBrain} />
+        </div>
+        <div class="score-rows">
+          <FontAwesomeIcon style={{ color: "green" }} icon={faComment} />
+        </div>
+        <div class="score-rows">
+          <FontAwesomeIcon style={{ color: "blue" }} icon={faEye} />
+        </div>
+      </div>
+      <button>Get Random Score</button>
+      <button>Continue</button>
+    </div>
+  );
+};
